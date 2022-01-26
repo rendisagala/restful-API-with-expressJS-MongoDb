0fc6be9c2f7adbb6786dc6f93f99d9d991323b6f
rendisagala <rendisagala19@gmail.com>
2022-01-26 20:03:27 +0700

    maximize pages view

--- app.mjs
+++ app.mjs
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
 
@@ -15,8 +14,10 @@ const __dirname = dirname(__filename);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views/pages"));
 //serving static files (css,etc)
+
 app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public/js")));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
 //database
 import mongoose from "mongoose";
@@ -38,9 +39,13 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
-app.get("login/index", (req, res) => {
+app.get("/index", (req, res) => {
   res.render("index.ejs");
 });
+app.post("/create"),
+  (req, res) => {
+    res.render("create.ejs");
+  };
 
 //router
 import { router } from "./router/router.mjs";
